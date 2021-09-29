2ccfac2a5496eb40feedecf43c147a1042bebaec
Rapak <ezesinachijim@gmail.com>
2021-09-29 19:10:24 +0000

    completed movie api

--- script.js
+++ script.js
@@ -1,90 +1,149 @@
 // this is the list of all the movies in the store
-let AllMovies = [
+var Movies = [
 
 ];
 
-// this is the list of movies that have been rented
-let Movies_on_Rent = [
+var Users = [
 
 ];
 
-let User = [
+var userCount = 0;
+var movieCount = 0;
 
-];
 
+// User class
+class User {
+  constructor (member_id, surname, firstname, address, phone_no, email) {
+    this.id = member_id;
+    this.surname = surname;
+    this.firstname = firstname;
+    this.address = address;
+    this.phone_no = phone_no;
+    this.email = email;
+    this.movies = [];
+  }
 
-// storage of the data
-// storing his movies
-// his 
+  addMovie (movie) {
+    this.movies.push(movie);
+  }
 
-// class
-const user = {
-  constructor(member_id, surname, firstname, address, phone_no, email) {
-    this.member_id = member_id
-    this.surname = surname
-    this.firstname = firstname
-    this.address = address
-    this.phone_no = phone
-    this.email = email
+  returnMovie (movie) {
+    let newListOfMovies = this.movies.filter(item => item.id != movie.id);
+
+    this.movies = newListOfMovies;
+  }
+
+  get allUserMovies () {
+    return this.movies;
   }
 }
 
-class movie{
-  constructor(movie_id, movie_name, distributor, director, genre, rating, year_of_release, rental_cost, copiesExisting, instances) {
-    this.movie_id = movie_id
-    this.movie_name = movie_name
-    this.distributor = distributor
-    this.director = director
-    this.genre = genre
-    this.rating = rating
-    this.year_of_release = year_of_release
-    this.rental_cost = rental_cost
-    this.instances = instances
-    this.count = function(){
-      instances
-    }
+class Movie {
+  constructor (movie_id, movie_name, distributor, director, genre, rating, year_of_release, rental_price) {
+    this.id = movie_id;
+    this.name = movie_name;
+    this.distributor = distributor;
+    this.director = director;
+    this.genre = genre;
+    this.rating = rating;
+    this.yearOfRelease = year_of_release;
+    this.rentalPrice = rental_price;
+    this.rentIds = [];
   }
-  // counter
 
-
+  deleteMovie () {
+    delete this;
+  }
+  
+  get getMovie () {
+    return this;
+  }
 }
-
-
-
-
-
-
-// function that helps rent a movie
-// name
-
-// function that helps return a movie
-
-// funciton that edits the movies in the store
-
-// funciton that deletes the movies in the store
-
-XUo?
-
-What a
-
-
 
 
 // function that creates movies in the store
-
-function createNewMovie (movie_id, movie_name, distributor, director, genre, rating, year_of_release, rental_cost, instances) {
-    let newMovie = new movie(movie_id, movie_name, distributor, director, genre, rating, year_of_release, rental_cost, instances);
-    AllMovies.push(newMovie);
-  }
-
-
-// add some moves to the AllMovies  
-
-createNewMovie(0001, "Avenger age of utron", "marvel", "stephen spilberg", "action", 5, 2013, 2000, 10);
-createNewMovie(0002, "Avenger end game", "marvel", "stephen spilberg", "action", 4, 2015, 1000, 5);
+function addNewMovie (movie_id, movie_name, distributor, director, genre, rating, year_of_release, rental_cost) {
+    let newMovie = new Movie(movie_id, movie_name, distributor, director, genre, rating, year_of_release, rental_cost);
+    Movies.push(newMovie);
+    movie_id++;
+}
 
 
-console.log(AllMovies);
-// const end_game = new movie (0002, "Avenger end game", "marvel", "stephen spilberg", "action", 5, 2019, 2000);
-// const loki = new movie (0001, "Avenger loki", "marvel", "stephen spilberg", "action", 5, 2013, 2000);
-// const infinity_wars = new movie (0001, "Avenger infinity wars", "marvel", "stephen spilberg", "action", 5, 2013, 2000);
+// function that creates new user
+function addNewUser (member_id, surname, firstname, address, phone_no, email) {
+  let newUser = new User(member_id, surname, firstname, address, phone_no, email);
+  Users.push(newUser);
+  member_id++;
+}
+
+
+// function that helps rent a movie
+function rentMovie (userId, movieId) {
+  let movie;
+  Movies.forEach(function (item) {
+    if (item.id == movieId) {
+      movie = item;
+      item.rentIds.push(userId);
+    }
+  });
+  Users.forEach(function (s) {
+    if (s.id == userId) {
+      s.addMovie(movie);
+    }
+  });
+}
+
+
+// function that helps return a movie
+function returnMovie (userId, movieId) {
+  let movie;
+  Movies.forEach(function (item) {
+    if (item.id == movieId) {
+      movie = item;
+      let id = item.rentIds.indexOf(userId);
+      item.rentIds.splice(id, 1);
+    }
+  });
+  Users.forEach(function (s) {
+    if (s.id == userId) {
+      s.returnMovie(movie);
+    }
+  });
+}
+
+// function that deletes the movies in the store
+function removeMovie (movieId) {
+  let movie;
+  Movies.forEach(function (item) {
+    if (item.id == movieId) {
+      movie = item;
+    }
+  });
+
+  let newMovies = Movies.filter(s => s.id != movie.id);
+  Movies = newMovies;
+
+  Users.forEach(function (s) {
+    for (const i of s.movies) {
+      if (i.id == movieId) {
+        s.returnMovie(movie);
+      }
+    }
+  });
+}
+
+addNewMovie(movieCount, "The Social Network", "Columbia Pictures", "David Fincher", "Biography", 5, 2010, 500);
+
+addNewUser(userCount, "Ezesinachi", "Jim", "4131 Faber Drive", 8164990885, "ezesinachijim@gmail.com");
+
+rentMovie(0, 0);
+console.log(Movies);
+console.log(Users);
+
+returnMovie(0, 0);
+console.log(Movies);
+console.log(Users);
+
+removeMovie(0);
+console.log(Movies);
+console.log(Users);
